98c770fbdc8251b4057f6685f43bca56bddc5f0d
Jensebluemchen4 <78657534+Jensebluemchen4@users.noreply.github.com>
2023-02-13 22:54:01 +0100

    add coolstuff

--- BasketBeans2D/Assets/Scripts/pickUp.cs
+++ BasketBeans2D/Assets/Scripts/pickUp.cs
@@ -19,20 +19,24 @@ public class pickUp : MonoBehaviour
 
     void Update()
     {
+
         RaycastHit2D isHit = Physics2D.Raycast(ray.position, transform.right, distance);
 
-        if (Input.GetKeyDown(KeyCode.E) && pickedUpObject == null)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && pickedUpObject == null)
         {
             
             pickedUpObject = isHit.collider.gameObject;
             pickedUpObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            pickedUpObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            pickedUpObject.GetComponent<Collider2D>().enabled = false;
             pickedUpObject.transform.position = hold.position;
             pickedUpObject.transform.SetParent(transform);
         }
-        else if (Input.GetKeyDown(KeyCode.E))
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             pickedUpObject.GetComponent<Rigidbody2D>().isKinematic = false;
             pickedUpObject.transform.SetParent(null);
+            pickedUpObject.GetComponent<Collider2D>().enabled = true;
             pickedUpObject = null;
         }
 
