dd0d44aaa407eb0a14c0fb7015f855ba5677610e
Jensebluemchen4 <78657534+Jensebluemchen4@users.noreply.github.com>
2023-02-16 13:50:22 +0100

    Add all

---
+++ BasketBeans2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce;
+    public float moveSpeed;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    private bool onGround;
+    public LayerMask isGround;
+    public Transform groundCheck;
+    public float radiusCheck;
+
+    Vector3 mousePosition;
+    bool lookRight;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (onGround == true && Input.GetButtonDown("Jump"))
+        {
+                rb.velocity = Vector2.up * jumpForce;
+        }
+        FollowMouse();
+        
+        if(lookRight == true && rb.position.x < mousePosition.x)
+        {
+            Flip();
+        }
+        else if(lookRight == false && rb.position.x > mousePosition.x)
+        {
+            Flip();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, radiusCheck, isGround);
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    void FollowMouse()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    void Flip()
+    {
+        lookRight = !lookRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
---
+++ BasketBeans2D/Assets/Scripts/pickUp.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour
+{
+    [SerializeField] private Transform hold;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private Rigidbody2D ballrb;
+    [SerializeField] private float maxDist = 0.5f;
+    [SerializeField] private float throwForce = 25f;
+    private bool inHand = false;
+    private float maxRadius = 10f;
+    
+    Vector3 mousePosition;
+
+    void Update()
+    {
+        FollowMouse();
+
+        Physics2D.IgnoreLayerCollision(0, 3);
+
+        //Check Distance between ball and player
+        float distance = Vector2.Distance(ball.transform.position, transform.position);
+
+        Vector3 throwDir = mousePosition - transform.position;
+
+        //Check if ball is not in hand and nearby
+        if (Input.GetKeyDown(KeyCode.Mouse0) && inHand == false && distance <= maxDist)
+        {
+            PickUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0) && inHand == true)
+        {
+            Throw(throwDir);
+        }
+    }
+
+    //Method to pick up ball
+    void PickUp()
+    {
+        ball.GetComponent<Rigidbody2D>().isKinematic = true;
+        ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+
+        //Move object to transform of object "hold" and set player to Parent
+        ball.transform.position = hold.position;
+        ball.transform.SetParent(transform);
+        ballrb.freezeRotation = true;
+
+        inHand = true;
+    }
+
+    //Method to throw ball
+    void Throw(Vector3 throwDir)
+    {
+        ball.GetComponent<Rigidbody2D>().isKinematic = false;
+        ball.transform.SetParent(null);
+
+        ballrb.AddForce(Vector3.ClampMagnitude((throwDir), maxRadius) * throwForce);
+
+        ballrb.freezeRotation = false;
+
+        inHand = false;
+    }
+
+    void FollowMouse()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
+
+
